98032c010aeaeab818d5d2b564a34d927117b1ff
Nikki Veen <nikkiveen@gmail.com>
2016-05-01 10:38:12 -0500

    nikki's stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'products#index'
   
   get '/products' => 'products#index'
+  get '/omgwutholycow' => 'omg_wut_holy_cow#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
